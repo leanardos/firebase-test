f941ad5147b3a392bd9eaf469d70caebd0d10b91
Anar <sadiglianar@gmail.com>
2021-04-30 23:44:13 +0400

    subscription on AuthStateChanged

--- public/app.js
+++ public/app.js
@@ -12,3 +12,19 @@ const provider = new firebase.auth.GoogleAuthProvider();
 
 signInBtn.onclick = () => auth.signInWithPopup(provider);
 signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if (user)
+    {
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3> Hello ${user.displayName}! </h3>`;
+    } 
+    else
+    {
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = ``;
+    }
+});
+
